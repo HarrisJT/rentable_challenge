f25d4da9126d92e10ed9f88a7da1c48e59a9fc7b
Harris Thompson <harris@harrisjt.com>
2021-08-26 11:01:19 -0500

    initial commit

---
+++ src/gilded_rose.rb
@@ -0,0 +1,51 @@
+class DewDropInn
+  def process_end_of_day(items)
+    items.each { |item| process_item_end_of_day item }
+  end
+
+  def process_item_end_of_day(item)
+    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.quality > 0
+        if item.name != "Sulfuras, Hand of Ragnaros"
+          item.quality = item.quality - 1
+        end
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.days_remaining < 11
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
+          end
+          if item.days_remaining < 6
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
+          end
+        end
+      end
+    end
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.days_remaining = item.days_remaining - 1
+    end
+    if item.days_remaining < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality > 0
+            if item.name != "Sulfuras, Hand of Ragnaros"
+              item.quality = item.quality - 1
+            end
+          end
+        else
+          item.quality = item.quality - item.quality
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+    end
+  end
+end
---
+++ test/gilded_rose_spec.rb
@@ -0,0 +1,293 @@
+require_relative "./spec_helper"
+require_relative "../src/drop_inn"
+require_relative "../src/item"
+
+RSpec.describe DewDropInn do
+  context "Normal Item" do
+    it "before sell date" do
+      items = [Item.new(name: "foo", days_remaining: 5, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 9)
+    end
+
+    it "with min quality" do
+      items = [Item.new(name: "foo", days_remaining: 5, quality: 0)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 0)
+    end
+
+    it "on sell date" do
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 8)
+    end
+
+    it "on sell date with min quality" do
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 0)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
+    end
+
+    it "on sell date near min quality" do
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 1)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
+    end
+
+    it "after sell date" do
+      items = [Item.new(name: "foo", days_remaining: -10, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 8)
+    end
+
+    it "after sell date with min quality" do
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 0)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
+    end
+
+    it "after sell date near min quality" do
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 1)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
+    end
+  end
+
+  context "Aged Brie" do
+    it "before sell date" do
+      items = [Item.new(name: "Aged Brie", days_remaining: 5, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 11)
+    end
+
+    it "with max quality" do
+      items = [Item.new(name: "Aged Brie", days_remaining: 5, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 50)
+    end
+
+    it "on sell date" do
+      items = [Item.new(name: "Aged Brie", days_remaining: 0, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 12)
+    end
+
+    it "on sell date with max quality" do
+      items = [Item.new(name: "Aged Brie", days_remaining: 0, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 50)
+    end
+
+    it "on sell date near max quality" do
+      items = [Item.new(name: "Aged Brie", days_remaining: 0, quality: 49)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 50)
+    end
+
+    it "after sell date" do
+      items = [Item.new(name: "Aged Brie", days_remaining: -10, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 12)
+    end
+
+    it "after sell date with max quality" do
+      items = [Item.new(name: "Aged Brie", days_remaining: -10, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 50)
+    end
+
+    it "after sell date near max quality" do
+      items = [Item.new(name: "Aged Brie", days_remaining: -10, quality: 49)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 50)
+    end
+  end
+
+  context "Sulfuras" do
+    it "before sell date" do
+      items = [Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 80)
+    end
+
+    it "on sell date" do
+      items = [Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 80)
+    end
+
+    it "after sell date" do
+      items = [Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -10, quality: 80)
+    end
+  end
+
+  context "Backstage Pass" do
+    it "long before sell date" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 10, quality: 11)
+    end
+
+    it "long before sell date at max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 10, quality: 50)
+    end
+
+    it "medium close to sell date upper bound" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 9, quality: 12)
+    end
+
+    it "medium close to sell date upper bound at max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 9, quality: 50)
+    end
+
+    it "medium close to sell date upper bound near max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 49)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 9, quality: 50)
+    end
+
+    it "medium close to sell date lower bound" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 12)
+    end
+
+    it "medium close to sell date lower bound at max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 50)
+    end
+
+    it "medium close to sell date lower bound near max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 49)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 50)
+    end
+
+    it "very close to sell date upper bound" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 13)
+    end
+
+    it "very close to sell date upper bound at max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 50)
+    end
+
+    it "very close to sell date upper bound near max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 48)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 50)
+    end
+
+    it "very close to sell date lower bound" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 13)
+    end
+
+    it "very close to sell date lower bound at max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 50)
+    end
+
+    it "very close to sell date lower bound near max quality" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 48)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 50)
+    end
+
+    it "on sell date" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
+    end
+
+    it "after sell date" do
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)]
+
+      DewDropInn.new.process_end_of_day(items)
+
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 0)
+    end
+  end
+end
